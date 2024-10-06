0ceb107e96aac9f15d9f0924873c68767f2ffc73
PouplarWesel <94089618+PouplarWesel@users.noreply.github.com>
2024-10-06 02:49:21 -0700

    Adding two levels

--- door.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-@onready var doorCollider = %DoorCollision
-
-func _on_door_area_2d_body_entered(body):
-	if(body.name.contains("SneakyCharacter")):
-		if(body.holdItemName.contains("key")):
-			print("OPEN DOOR")
-			doorCollider.disabled = true
